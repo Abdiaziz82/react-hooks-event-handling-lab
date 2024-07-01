05d64069ed0de6c83081151b95efc64d4e9bee0a
Abdiaziz82 <abdiazizhared64@gmail.com>
2024-07-01 08:31:25 +0300

    complete event-lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+function EyesOnMe(){
+    function HandleFOcus(){
+        console.log('Good!')
+    }
+
+    function HandleBlur(){
+        console.log('Hey! Eyes on me!')
+    }
+
+    return(
+        <div>
+
+            <button onBlur={HandleBlur}      onFocus ={HandleFOcus}  >Eyes on me</button>
+            </div>
+    )
+}
+export default EyesOnMe
